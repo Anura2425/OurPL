4bd882152b3cc1ffb7a0db7553895eba32626081
Anura2425 <gmcclurecoleman@gmail.com>
2026-04-19 13:49:51 -0700

    added new ASTPrinter

--- src/main/java/cpsc326/ASTPrinter.java
+++ src/main/java/cpsc326/ASTPrinter.java
@@ -1,8 +1,13 @@
 package cpsc326;
 
-class ASTPrinter implements Expr.Visitor<String> {
+class ASTPrinter implements Expr.Visitor<String>{
     String print(Expr expr) {
         return expr.accept(this);
+    }
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return parenthesize("assign " + expr.name.lexeme, expr.value);
     }
 
     @Override
@@ -17,9 +22,13 @@ class ASTPrinter implements Expr.Visitor<String> {
 
     @Override
     public String visitLiteralExpr(Expr.Literal expr) {
-        if (expr.value == null)
-            return "nil";
+        if(expr.value == null) return "nil";
         return expr.value.toString();
+    }
+
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
     @Override
@@ -27,14 +36,20 @@ class ASTPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
         builder.append("(").append(name);
-        for (Expr expr : exprs) {
+        for(Expr expr : exprs) {
             builder.append(" ");
             builder.append(expr.accept(this));
         }
         builder.append(")");
+
         return builder.toString();
     }
 }
