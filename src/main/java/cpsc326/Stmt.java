e1e484c2643d415629fa4f2cb8b29e3efaa71bd1
Anura2425 <gmcclurecoleman@gmail.com>
2026-04-19 14:15:11 -0700

    Stmt.java updated with new subclasses (while if block)

--- src/main/java/cpsc326/Stmt.java
+++ src/main/java/cpsc326/Stmt.java
@@ -1,10 +1,53 @@
 package cpsc326;
+import java.util.List;
 
 abstract class Stmt {
     interface Visitor<R>{
         R visitPrintStatement(Stmt.Print Stmt);
         R visitExpressionStatement(Stmt.Expression Stmt);
         R visitVarDeclaration(Stmt.VarDecl Stmt);
+        R visitBlockStatement(Stmt.Block Stmt);
+        R visitIfStatement(Stmt.If Stmt);
+        R visitWhileStatement(Stmt.While Stmt);
+    }
+
+    static class Block extends Stmt {
+        final List<Stmt> stmt;
+        Block(List<Stmt> stmts){
+            stmt = stmts;
+        }
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBlockStatement(this);
+        }
+    }
+
+    static class If extends Stmt{
+        final Expr condition;
+        final Stmt thenBranch;
+        final Stmt elseBranch;
+        If(Expr cond, Stmt thenBr, Stmt elseBr){
+            condition = cond;
+            thenBranch = thenBr;
+            elseBranch = elseBr;
+        }
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitIfStatement(this);
+        }
+    }
+
+    static class While extends Stmt{
+        final Expr condition;
+        final Stmt body;
+        While(Expr cond, Stmt bod){
+            condition = cond;
+            body = bod;
+        }
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitWhileStatement(this);
+        }
     }
 
     static class Print extends Stmt {
